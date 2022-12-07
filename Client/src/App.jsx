b785267917c8b87aa87ba18f5dec79336c0c4eba
Alex Minnelli <alexminnelli@gmail.com>
2022-12-07 12:51:11 -0500

    Editable fields on display

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,21 +9,28 @@ export function App() {
   let [rowData, setRowData] = useState();
   useEffect(() => {Axios.get('http://localhost:5100/people').then((res) => {setRowData(res.data)})}, [])
   
-  const actionButton = (params) => {
+  const deleteButton = (params) => {
     Axios.delete(`http://localhost:5100/delete/${params.data.id}`)
     alert(`Person #${params.data.id} has been deleted`)
     Axios.get('http://localhost:5100/people').then((res) => {setRowData(res.data)})
   }
 
+  const edit = (column, params) => {
+    let ans = prompt(`Edit the ${column}`)
+    Axios.put(`http://localhost:5100/people/update/${column}/${params.data.id}`, {[column]: ans})
+    alert(`Person #${params.data.id}'s ${column} has been updated`)
+    Axios.get('http://localhost:5100/people').then((res) => {setRowData(res.data)})
+  }
+
   const columnDefs = [
-    { field: "id", width: 100},
-    { field: "first_name", cellRenderer: (params) => <div>{params.data.first_name} <button type='button'><i class="fa fa-pencil"></i></button></div>},
-    { field: "last_name", cellRenderer: (params) => <div>{params.data.last_name} <button type='button'><i class="fa fa-pencil"></i></button></div>},
-    { field: "image", width:100, cellRenderer: (params) => <div><img href={params.data.image} alt='Person'></img></div>},
-    { field: "email", width:400, cellRenderer: (params) => <div>{params.data.email} <button type='button'><i class="fa fa-pencil"></i></button></div>},
-    { field: "city", cellRenderer: (params) => <div>{params.data.city} <button type='button'><i class="fa fa-pencil"></i></button></div>},
-    { field: "country", cellRenderer: (params) => <div>{params.data.country} <button type='button'><i class="fa fa-pencil"></i></button></div>},
-    { headerName: 'Delete', width: 100, cellRenderer: (params) => <div><button type='button' onClick={()=>actionButton(params)}><i class="fa fa-trash"></i></button></div>}
+    {field: "id", width: 100},
+    {field: "first_name", cellRenderer: (params) => <div>{params.data.first_name} <button type='button' onClick={()=>edit('first_name', params)}><i className="fa fa-pencil"></i></button></div>},
+    {field: "last_name", cellRenderer: (params) => <div>{params.data.last_name} <button type='button' onClick={()=>edit('last_name', params)}><i className="fa fa-pencil"></i></button></div>},
+    {field: "image", width:100, cellRenderer: (params) => <div><img href={params.data.image} alt='Person'></img></div>},
+    {field: "email", width:300, cellRenderer: (params) => <div>{params.data.email} <button type='button' onClick={()=>edit('email', params)}><i className="fa fa-pencil"></i></button></div>},
+    {field: "city", cellRenderer: (params) => <div>{params.data.city} <button type='button' onClick={()=>edit('city', params)}><i className="fa fa-pencil"></i></button></div>},
+    {field: "country", cellRenderer: (params) => <div>{params.data.country} <button type='button' onClick={()=>edit('country', params)}><i className="fa fa-pencil"></i></button></div>},
+    {headerName: 'Delete', width: 100, cellRenderer: (params) => <div><button type='button' onClick={()=>deleteButton(params)}><i className="fa fa-trash"></i></button></div>}
   ];
 
   const defaultColDef = {
--- Server/index.mjs
+++ Server/index.mjs
@@ -33,6 +33,33 @@ app.get('/people', async(req, res, next) => {
     }catch(err){next(err)}
 })
 
+app.put('/people/update/first_name/:id', async(req, res, next) => {
+    try{res.status(200).send(await query('UPDATE mock_data SET first_name = ? WHERE id = ?', [req.body.first_name, req.params.id]))
+    }catch(err){next(err)}
+})
+
+app.put('/people/update/last_name/:id', async(req, res, next) => {
+    try{res.status(200).send(await query('UPDATE mock_data SET last_name = ? WHERE id = ?', [req.body.last_name, req.params.id]))
+    }catch(err){next(err)}
+})
+
+app.put('/people/update/email/:id', async(req, res, next) => {
+    try{res.status(200).send(await query('UPDATE mock_data SET email = ? WHERE id = ?', [req.body.email, req.params.id]))
+    }catch(err){next(err)}
+})
+
+app.put('/people/update/city/:id', async(req, res, next) => {
+    try{res.status(200).send(await query('UPDATE mock_data SET city = ? WHERE id = ?', [req.body.city, req.params.id]))
+    }catch(err){next(err)}
+})
+
+app.put('/people/update/country/:id', async(req, res, next) => {
+    try{res.status(200).send(await query('UPDATE mock_data SET country = ? WHERE id = ?', [req.body.country, req.params.id]))
+    }catch(err){next(err)}
+})
+
+
+
 app.delete('/delete/:id', async (req, res, next) => {
     try{res.status(200).send(await query('DELETE FROM mock_data WHERE id = ?', [req.params.id]))
     }catch(err){next(err)}
